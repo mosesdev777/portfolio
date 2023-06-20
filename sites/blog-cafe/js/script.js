e1ca531097207231734f35533fc43a095cdc0970
Moises Sanchez <123977268+mosesdev777@users.noreply.github.com>
2023-06-20 10:46:52 -0400

    Subiendo el sitio Blog Cafe
    Se ha subido el sitio blog-cafe

---
+++ sites/blog-cafe/js/script.js
@@ -0,0 +1,6 @@
+console.log('Archivo script vinculado correctamente');
+
+let heading = document.querySelector('.header');
+
+heading.addEventListener('click', function (e) {  console.log(e.target); });
+
